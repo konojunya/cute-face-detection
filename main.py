dba320a1bcdd85a3ede9b624bc4b27bd3c1a5f04
konojunya <junya03dance@gmail.com>
2017-03-25 20:47:49 +0900

    :+1: images.txtを元に1枚ずつに顔認識をかけて顔がない画像は弾いて、顔がある画像はdownloadしてくる

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from utils.faceDetection import faceDetection as fd
+import sys, urllib
+import os.path
 
 if __name__ == "__main__":
-  fd.checkFaceDetection()
+  f = open("./images.txt","r")
+
+  for line in f:
+    url = str.strip(line)
+    isFace = fd.recognition(url)
+    
+    if not(isFace):
+      os.remove("assets/"+os.path.basename(url))
+
+  f.close()
--- utils/faceDetection.py
+++ utils/faceDetection.py
@@ -1,56 +1,56 @@
 # -*- coding: utf-8 -*-
-import cv2
-import os.path
+import cv2, os.path, sys, urllib
+
 
 class FaceDetection(object):
 
-  def __init__(self):
-    self.image_path = "./girl.jpg"
-    self.cascade_path = {
-      "face": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml",
-      "eye": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_eye.xml",
-      "nose": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_mcs_nose.xml"
-    }
+    def __init__(self):
+        self.cascade_path = {
+            "face": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml",
+            "eye": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_eye.xml",
+            "nose": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_mcs_nose.xml"
+        }
 
-  def readImage(self):
-    return cv2.imread(self.image_path)
+    def readImage(self,url):
+        img = urllib.urlopen(url)
+        image_path = os.path.basename(url)
+        localfile = open("assets/"+image_path, 'wb')
+        localfile.write(img.read())
+        img.close()
+        localfile.close()
 
-  def convertToGrayScale(self,image):
-    return cv2.cvtColor(image,cv2.cv.CV_BGR2GRAY)
+        return cv2.imread("assets/"+image_path)
 
-  def recognition(self):
-    image = self.readImage()
-    image_gray = self.convertToGrayScale(image)
+    def convertToGrayScale(self, image):
+        return cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
 
-    face_cascade = cv2.CascadeClassifier(self.cascade_path["face"])
-    facerect = face_cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-    
-    if len(facerect) > 0:
-      eye_cascade = cv2.CascadeClassifier(self.cascade_path["eye"])
-      eyerect = eye_cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-      nose_cascade = cv2.CascadeClassifier(self.cascade_path["nose"])
-      noserect = nose_cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+    def recognition(self,url):
+        image = self.readImage(url)
+        image_gray = self.convertToGrayScale(image)
 
-    facerect_len = len(facerect)
-    eyerect_len = len(eyerect)
-    noserect_len = len(noserect)
+        face_cascade = cv2.CascadeClassifier(self.cascade_path["face"])
+        facerect = face_cascade.detectMultiScale(
+            image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-    return int(facerect_len)+int(eyerect_len)+int(noserect_len) > 10 if True else False
+        eyerect = []
+        noserect = []
 
+        if len(facerect) > 0:
+            eye_cascade = cv2.CascadeClassifier(self.cascade_path["eye"])
+            eyerect = eye_cascade.detectMultiScale(
+                image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+            nose_cascade = cv2.CascadeClassifier(self.cascade_path["nose"])
+            noserect = nose_cascade.detectMultiScale(
+                image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-  def checkFaceDetection(self):
-    isFace = self.recognition()
+        facerect_len = len(facerect)
+        eyerect_len = len(eyerect)
+        noserect_len = len(noserect)
 
-    if isFace:
-      print "This image is face!"
-    else:
-      print "This image is not face!"
+        print "\nface rect/ "+str(facerect_len)
+        print "eye rect/ "+str(eyerect_len)
+        print "nose rect/ "+str(noserect_len)
 
-  def exportDetectedImage(self,rects):
-    img = self.readImage()
-    for rect in rects:
-      cv2.rectangle(img, tuple(rect[0:2]),tuple(rect[0:2]+rect[2:4]), (0,0,0), thickness=2)
-      cv2.imwrite("detected.jpg", img)
-
+        return int(facerect_len) + int(eyerect_len) + int(noserect_len) > 10 if True else False
 
 faceDetection = FaceDetection()
