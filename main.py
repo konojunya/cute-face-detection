386a6157fefe6fbac8d92a2bfd94d97dc3e61147
konojunya <junya03dance@gmail.com>
2017-03-27 14:20:00 +0900

    :+1: pep8で整形

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 from utils.faceDetection import faceDetection as fd
-import sys, urllib
+import sys
+import urllib
 import os.path
 import json
 
 if __name__ == "__main__":
 
-  print "\nstarting face detection!\n\n"
+    print "\nstarting face detection!\n\n"
 
-  f = open("./images.txt","r")
-  jsonData = {}
+    f = open("./images.txt", "r")
+    jsonData = {}
 
-  for line in f:
-    url = str.strip(line)
-    path = os.path.basename(url)
-    jsonData[path] = fd.recognition(url)
+    for line in f:
+        url = str.strip(line)
+        path = os.path.basename(url)
+        jsonData[path] = fd.recognition(url)
 
-  f.close()
+    f.close()
 
-  f = open("assets.json","w")
-  json.dump(jsonData,f)
-  f.close()
+    f = open("assets.json", "w")
+    json.dump(jsonData, f)
+    f.close()
 
-  print "complete!"
+    print "complete!"
--- utils/faceDetection.py
+++ utils/faceDetection.py
@@ -7,6 +7,7 @@ import numpy as np
 
 print WARNING + 'Warning' + ENDC
 
+
 class FaceDetection(object):
 
     def __init__(self):
@@ -16,53 +17,54 @@ class FaceDetection(object):
             "nose": "/usr/local/opt/opencv/share/OpenCV/haarcascades/haarcascade_mcs_nose.xml"
         }
 
-    def print(self,text):
-      print '\033[92m' + text +  '\033[0m'
+    def print(self, text):
+        print '\033[92m' + text + '\033[0m'
 
-    def readImage(self,url):
-      print "downloading %s" % (url)
-      resp = urllib.urlopen(url)
-      image = np.asarray(bytearray(resp.read()),dtype="uint8")
-      image = cv2.imdecode(image,cv2.IMREAD_COLOR)
+    def readImage(self, url):
+        print "downloading %s" % (url)
+        resp = urllib.urlopen(url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-      return image
+        return image
 
     def convertToGrayScale(self, image):
-      return cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+        return cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
 
-    def recognition(self,url):
-      try:
-        image = self.readImage(url)
-        image_gray = self.convertToGrayScale(image)
+    def recognition(self, url):
+        try:
+            image = self.readImage(url)
+            image_gray = self.convertToGrayScale(image)
 
-        face_cascade = cv2.CascadeClassifier(self.cascade_path["face"])
-        facerect = face_cascade.detectMultiScale(
-            image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-
-        eyerect = []
-        noserect = []
-
-        if len(facerect) > 0:
-            eye_cascade = cv2.CascadeClassifier(self.cascade_path["eye"])
-            eyerect = eye_cascade.detectMultiScale(
-                image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-            nose_cascade = cv2.CascadeClassifier(self.cascade_path["nose"])
-            noserect = nose_cascade.detectMultiScale(
+            face_cascade = cv2.CascadeClassifier(self.cascade_path["face"])
+            facerect = face_cascade.detectMultiScale(
                 image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-        facerect_len = len(facerect)
-        eyerect_len = len(eyerect)
-        noserect_len = len(noserect)
+            eyerect = []
+            noserect = []
 
-        self.print("\nface rect:\t\t"+str(facerect_len))
-        self.print("eye rect:\t\t"+str(eyerect_len))
-        self.print("nose rect:\t\t"+str(noserect_len))
-        print "\n"
-      except:
-        facerect_len = 0
-        eyerect_len = 0
-        noserect_len = 0
+            if len(facerect) > 0:
+                eye_cascade = cv2.CascadeClassifier(self.cascade_path["eye"])
+                eyerect = eye_cascade.detectMultiScale(
+                    image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+                nose_cascade = cv2.CascadeClassifier(self.cascade_path["nose"])
+                noserect = nose_cascade.detectMultiScale(
+                    image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-      return int(facerect_len) + int(eyerect_len) + int(noserect_len) > 10 if True else False
+            facerect_len = len(facerect)
+            eyerect_len = len(eyerect)
+            noserect_len = len(noserect)
+
+            self.print("\nface rect:\t\t" + str(facerect_len))
+            self.print("eye rect:\t\t" + str(eyerect_len))
+            self.print("nose rect:\t\t" + str(noserect_len))
+            print "\n"
+        except:
+            facerect_len = 0
+            eyerect_len = 0
+            noserect_len = 0
+
+        return int(facerect_len) + int(eyerect_len) + int(noserect_len) > 10 if True else False
+
 
 faceDetection = FaceDetection()
